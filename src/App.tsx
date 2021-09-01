09476b77aa9b3377e652cc47fcf7e39060fbefa0
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-09-01 10:37:10 +0800

    Add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-blue-400">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium
+      autem laboriosam iure facere ad itaque minus perferendis tempore ullam
+      aspernatur. Quasi, voluptatem officiis temporibus aperiam voluptatibus
+      doloremque odit asperiores voluptates!
     </div>
   );
 }
