6b4d52cff4879510df2748f2a1057974e3e7f4fa
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-09-01 14:15:50 +0800

    Design banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ChevronDoubleDownIcon } from "@heroicons/react/solid";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Header from "./components/header/Header";
@@ -7,10 +8,33 @@ const App = () => {
     <Router>
       <div className="font-oswald">
         <Header />
-        <main>
+        <main className="p-4">
           <Switch>
             <Route exact path="/">
-              <p>Home</p>
+              <section className="p-4 text-center">
+                <div id="banner" className="p-2 mb-4">
+                  <h3 className="text-3xl font-bold mb-16">
+                    Welcome to my Website
+                  </h3>
+
+                  <div className="w-1/3 -mb-2 mx-auto">
+                    <img
+                      src="https://picsum.photos/200"
+                      alt="user avatar"
+                      className="w-full object-cover rounded-full"
+                    />
+                  </div>
+
+                  <p className="p-2 bg-red-100 bg-opacity-50 rounded">
+                    I am Angelo Arcillas, an aspiring web developer from the
+                    philippines. I love back-end development and Cybersecurity.
+                  </p>
+                </div>
+
+                <div className="inline-block p-2 border-2 border-red-400 rounded-full">
+                  <ChevronDoubleDownIcon className="w-8 h-8 text-red-400" />
+                </div>
+              </section>
             </Route>
             <Route path="/profile">
               <p>profile</p>
