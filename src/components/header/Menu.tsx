eeba15534de72b3921ca0a18f8aa11e102995b08
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-09-01 14:59:56 +0800

    fix: dots not changing color when hovering the div

--- src/components/header/Menu.tsx
+++ src/components/header/Menu.tsx
@@ -7,8 +7,8 @@ type MenuProps = {
 const Menu = ({ toggleNavBar }: MenuProps) => {
   return (
     <div>
-      <div onClick={toggleNavBar} className="p-2 cursor-pointer">
-        <DotsVerticalIcon className="w-8 h-8 hover:text-red-400" />
+      <div onClick={toggleNavBar} className="p-2 cursor-pointer hover:text-red-400">
+        <DotsVerticalIcon className="w-8 h-8 text-current" />
       </div>
     </div>
   );
