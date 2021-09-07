4831016ebdf76d104620ea852d63c8dc322ebc9d
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-09-07 11:36:06 +0800

    test select none

--- src/components/header/Menu.tsx
+++ src/components/header/Menu.tsx
@@ -10,7 +10,7 @@ const Menu = ({ toggleNavBar }: MenuProps) => {
       onClick={toggleNavBar}
       className="py-1 cursor-pointer select-none hover:text-red-400"
     >
-      <MenuAlt3Icon className="w-12 h-8 text-current" />
+      <MenuAlt3Icon className="w-12 h-8 text-current select-none" />
     </div>
   );
 };
