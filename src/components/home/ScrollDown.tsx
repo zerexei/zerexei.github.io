f484dccfaffe6f63d875b52dee9c41003c3aae79
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-09-07 11:01:25 +0800

    refactor: remove user select

--- src/components/home/ScrollDown.tsx
+++ src/components/home/ScrollDown.tsx
@@ -2,7 +2,7 @@ import { ChevronDoubleDownIcon } from "@heroicons/react/solid";
 
 const ScrollDown = () => {
   return (
-    <a href="#TopProjects" className="scroll-down">
+    <a href="#TopProjects" className="scroll-down select-none">
       <ChevronDoubleDownIcon className="w-8 h-8 text-current" />
     </a>
   );
