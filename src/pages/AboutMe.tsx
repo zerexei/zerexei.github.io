f18bdcd6603f12d11652ed4eba93ed692bb1c1a1
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-09-17 15:07:06 +0800

    Add dev.to badge

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,8 +1,16 @@
 const AboutMe = () => {
   return (
-    <div className="h-screen flex flex-col gap-5 items-center justify-center">
-      <h2 className="text-2xl text-red-400">About me page</h2>
-      <p className="animate-ping text-8xl">Comming soon</p>
+    <div className="min-h-screen flex items-center justify-center">
+      <a href="https://app.daily.dev/zerexei">
+        <div className="w-full lg:w-2/4 mx-auto p-6">
+          <img
+            src="https://api.daily.dev/devcards/563f335bff2443e5be44fe6292f1fc0c.png?r=1hk"
+            alt="Angelo Arcillas's Dev Card"
+            width="400"
+            className="w-full object-cover"
+          />
+        </div>
+      </a>
     </div>
   );
 };
