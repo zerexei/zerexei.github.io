abf51469de24a195bb1e5003b07730a4b5922911
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-03-24 14:38:54 +0800

    Add namd and role component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 function App() {
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div className="font-mono container mx-auto p-8">
+      // ---------------
+      <div className="flex items-center">
+        <p className="flex-1">
+          <h1 className="text-4xl font-bolder">John Doe</h1>
+          <h3 className="text-xl font-bold text-gray-500 tracking-wide">
+            Web Developer
+          </h3>
+        </p>
+        <div className="w-28">
+          <img
+            className="relative object-cover rounded-full"
+            src="https://picsum.photos/500"
+            alt="avatar"
+          />
+        </div>
+      </div>
     </div>
   );
 }
