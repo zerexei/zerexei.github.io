1f519d7e8215a9fff81077ad9d7a780d19cff76c
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-03-25 12:38:47 +0800

    refactor Connect component icon classname

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -7,28 +7,31 @@ import {
 } from "react-icons/fa";
 
 const Connect = () => {
+  const iconClassName = "w-8 h-8 text-red-400 hover:text-red-500";
+
   return (
-    <div className="flex items-center space-x-2 text-red-400">
+    <div className="flex items-center space-x-2">
       <a href="https://www.linkedin.com/in/angeloarcillas" alt="linkedin link">
-        <FaLinkedin className="w-8 h-8 hover:text-red-500" />
+        <FaLinkedin className={iconClassName} />
       </a>
 
       <a href="https://github.com/zerexei" alt="github link">
-        <FaGithub className="w-8 h-8 hover:text-red-500" />
+        <FaGithub className={iconClassName} />
       </a>
 
       <a href="#" alt="stack overflow link">
-        <FaStackOverflow className="w-8 h-8 hover:text-red-500" />
+        <FaStackOverflow className={iconClassName} />
       </a>
 
       <a href="#" alt="twitter link">
-        <FaTwitter className="w-8 h-8 hover:text-red-500" />
+        <FaTwitter className={iconClassName} />
       </a>
 
       <a href="#" alt="discord link">
-        <FaDiscord className="w-8 h-8 hover:text-red-500" />
+        <FaDiscord className={iconClassName} />
       </a>
     </div>
   );
 };
+
 export default Connect;
