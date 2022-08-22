5ae69d40bd7b05ef9a74599075cd07f15d197179
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-08-22 22:25:16 +0800

    Add bio

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,27 @@ function App() {
   return (
     <div className="App h-full bg-gray-50">
       <Header />
-      <main></main>
+      <main>
+        <section className="max-w-screen-md mx-auto mt-12 p-4">
+          <div className="flex flex-col items-center text-center">
+            <figure className="w-48 h-48 mb-4">
+              <img
+                className="rounded-full"
+                src="https://picsum.photos/500"
+                alt="avatar"
+              />
+            </figure>
+            <h2>Angelo Arcillas</h2>
+            <h3 className="mb-4">Web Developer</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+              doloribus amet nostrum distinctio saepe velit maiores accusantium
+              animi sapiente, omnis quo fugiat veniam doloremque porro tenetur,
+              excepturi inventore natus modi!
+            </p>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
