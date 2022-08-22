69ca916d123f123eb666e9996bbecc291a1ab2ec
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-08-22 21:57:27 +0800

    Add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import '@/assets/css/App.css';
+import './assets/css/App.css';
 
 function App() {
-  return <div className="App">
-    <header></header>
-    <main></main>
-  </div>;
+  return (
+    <div className="App h-full bg-gray-50">
+      <header className="flex items-center px-4 shadow-md">
+        <h2 className="flex-1 text-4xl">Foo Bar Baz</h2>
+        <nav className="flex-1 hidden md:flex text-center">
+          <a href="#a" className="nav-link nav-link--active">
+            Home
+          </a>
+          <a href="#a" className="nav-link">
+            Services
+          </a>
+          <a href="#a" className="nav-link">
+            Projects
+          </a>
+          <a href="#a" className="nav-link">
+            Tools
+          </a>
+        </nav>
+      </header>
+      <main></main>
+    </div>
+  );
 }
 
 export default App;
