e540cce938220f7cf803ef8475a31b5c606ba35b
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-12-26 08:02:03 +0800

    remove mobile nav

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import MenuIcon from '../../assets/icons/MenuIcon';
-import MobileNavigation from './MobileNavigation';
 import Navigation from './Navigation';
 
 const Header = () => {
@@ -13,13 +12,16 @@ const Header = () => {
   return (
     <header className="md:flex md:items-center p-4 md:py-0 shadow-md md:px-20">
       <div className="md:flex-1 flex justify-between">
-        <h2 className="mb-4 md:mb-0 text-4xl">FooBar</h2>
+        <a href="#">
+          <h1 className="mb-4 md:mb-0 text-2xl">
+            Angelo Arcillas - Software Engineer
+          </h1>
+        </a>
         <span className="md:hidden" onClick={() => toggleMobileNav()}>
           <MenuIcon css="w-8 h-8" />
         </span>
       </div>
       <Navigation />
-      <MobileNavigation active={mobileNav} />
     </header>
   );
 };
--- src/components/header/Navigation.tsx
+++ src/components/header/Navigation.tsx
@@ -1,18 +1,23 @@
+export const links = [
+  { label: 'Home', url: '#', isActive: true },
+  { label: 'Services', url: '#/services', isActive: false },
+  { label: 'Projects', url: 'https://github.com/zerexei', isActive: false },
+  { label: 'Tools', url: '#/tools', isActive: false },
+];
+
 const Navigation = () => {
   return (
     <nav className="hidden md:flex text-center">
-      <a href="#a" className="nav-link nav-link--active py-2 px-6 hover:text-green-700 hover:border-b-2 hover:border-green-400">
-        Home
-      </a>
-      <a href="#a" className="nav-link py-2 px-6 hover:text-green-700 hover:border-b-2 hover:border-green-400">
-        Services
-      </a>
-      <a href="#a" className="nav-link  py-2 px-6 hover:text-green-700 hover:border-b-2 hover:border-green-400">
-        Projects
-      </a>
-      <a href="#a" className="nav-link  py-2 px-6 hover:text-green-700 hover:border-b-2 hover:border-green-400">
-        Tools
-      </a>
+      {links.map((link) => {
+        return (
+          <a
+            href={link.url}
+            className={`nav-link ${link.isActive ? '' : 'nav-link--active'}`}
+          >
+            {link.label}
+          </a>
+        );
+      })}
     </nav>
   );
 };
