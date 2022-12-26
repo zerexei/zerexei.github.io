54c25206fd0f99777cefd36e1196095052023122
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-12-26 12:13:32 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    emptyOutDir: true,
+    outDir: './build',
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name].[ext]',
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].[ext]',
+      },
+    },
+  },
 });
