43c0f55a4bec860840e06ee697d7756163fc15cc
Angelo Arcillas <angeloarcillas64@gmail.com>
2023-06-17 23:54:34 +0800

    fix UI: technology pill

--- src/components/home/Technologies.tsx
+++ src/components/home/Technologies.tsx
@@ -9,11 +9,11 @@ const Technologies = () => {
       <div className="flex flex-wrap gap-2">
         {[1, 1, 1, 1, 1, 1, 1].map(() => {
           return (
-            <div className="px-6 py-4 cursor-pointer hover:text-red-300 border border-transparent hover:border-red-400 rounded-full flex items-center gap-2 text-sm">
-              <div className="bg-red-400 rounded-full p-2">
+            <div className="px-4 py-1 cursor-pointer hover:text-red-300 border border-transparent hover:border-red-400 rounded-full flex items-center gap-2 text-sm">
+              <div className="bg-red-400 rounded-full p-1.5">
                 <UpIcon />
               </div>
-              <div className="flex-1">JavaScript</div>
+              <div className="flex-1 text-sm">JavaScript</div>
             </div>
           );
         })}
