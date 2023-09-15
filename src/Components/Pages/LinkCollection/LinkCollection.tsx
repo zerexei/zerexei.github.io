e4986b5865cd29d96645a221fadefdd37ae12cea
Angelo Arcillas <angeloarcillas64@gmail.com>
2023-09-15 22:14:17 +0800

    Add icons | TODOs

--- src/Components/Pages/LinkCollection/LinkCollection.tsx
+++ src/Components/Pages/LinkCollection/LinkCollection.tsx
@@ -1,7 +1,29 @@
-import UpIcon from '@/Components/Includes/Icons/UpIcon';
+import ClickedIcon from '@/Components/Includes/Icons/ClickedIcon';
+import LaravelIcon from '@/Components/Includes/Icons/dev/LaravelIcon';
+import SassIcon from '@/Components/Includes/Icons/dev/SassIcon';
+import TailwindcssIcon from '@/Components/Includes/Icons/dev/TailwindcssIcon';
+import VuejsIcon from '@/Components/Includes/Icons/dev/VuejsIcon';
 import { useState } from 'react';
 
 const LinkCollection = () => {
+  /*
+   TODO:
+   - Add more icons
+   - design filters
+   - use real data
+      - use firebase
+   - review UI
+   - upload to prod
+
+   - create about me page
+   - update projects section
+      - use firebase
+   - improve technologies section
+
+   - update tools UI
+      - use firebase
+   - add more tools
+   */
   return (
     <div className="p-12">
       <div className="flex items-end">
@@ -53,7 +75,7 @@ const InputSearch = () => {
 
 const LinkItem = () => {
   return (
-    <div className="flex items-center justify-between p-4 border border-transparent rounded-md hover:border-indigo-400">
+    <div className="flex items-center justify-between p-4 border border-transparent rounded-md cursor-pointer hover:border-indigo-400">
       <div>
         <h3 className="mb-1 font-semibold cursor-pointer hover:text-indigo-600">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit.
@@ -68,8 +90,8 @@ const LinkItem = () => {
       <div className="flex items-center gap-2">
         <AvatarStack />
         <div className="flex items-center text-slate-400">
-          <p className="inline-block p-1.5 mr-1 rounded-full cursor-pointer hover:bg-indigo-600">
-            <UpIcon className="w-5 h-5" />
+          <p className="inline-block p-1.5 mr-1 rounded-full cursor-pointer hover:bg-indigo-600 transition-colors duration-150">
+            <ClickedIcon />
           </p>
           <span className="inline text-xs">64</span>
         </div>
@@ -80,35 +102,28 @@ const LinkItem = () => {
 
 const AvatarStack = () => {
   return (
-    <div className="flex -space-x-1">
-      <img
-        className="inline-block w-6 h-6 rounded-full"
-        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-        alt=""
-      />
-      <img
-        className="inline-block w-6 h-6 rounded-full"
-        src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-        alt=""
-      />
-      <img
-        className="inline-block w-6 h-6 rounded-full"
-        src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
-        alt=""
-      />
-      <img
-        className="inline-block w-6 h-6 rounded-full"
-        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-        alt=""
-      />
+    <div className="flex items-center">
+      <span className="p-2 rounded-full bg-slate-700">
+        <LaravelIcon />
+      </span>
+      <span className="p-2 rounded-full bg-slate-700">
+        <VuejsIcon />
+      </span>
+      <span className="p-2 rounded-full bg-slate-700">
+        <TailwindcssIcon />
+      </span>
+      <span className="p-2 rounded-full bg-slate-700">
+        <SassIcon />
+      </span>
+      <EllipsisVerticalIcon />
     </div>
   );
 };
 
 const ComboBox = () => {
-  const [isShow, setIshow] = useState();
+  const [isShow, setIsShow] = useState<boolean>();
   return (
-    <div className="">
+    <div className="relative">
       <label
         htmlFor="input-search"
         className="block mb-1 text-sm font-semibold tracking-wide text-slate-400"
@@ -116,15 +131,16 @@ const ComboBox = () => {
         Tags
       </label>
       <input
-      onClick={setIshow(!isShow)}
+        onClick={() => setIsShow(!isShow)}
         type="text"
         name="search"
         id="input-search"
         placeholder="tags..."
+        autoComplete="off"
         className="px-3.5 w-[15rem] py-2.5 text-sm rounded-md text-slate-700"
       />
       {isShow && (
-        <div className="p-2 mt-2 text-sm bg-white rounded text-slate-700">
+        <div className="absolute w-full p-2 mt-2 text-sm bg-white rounded text-slate-700">
           <ComboBoxItem />
           <ComboBoxItem />
           <ComboBoxItem />
@@ -136,9 +152,27 @@ const ComboBox = () => {
   );
 };
 
+const EllipsisVerticalIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+      />
+    </svg>
+  );
+};
 const ComboBoxItem = () => {
   return (
-    <label className="flex items-center p-1 cursor-pointer select-none hover:text-indigo-700">
+    <label className="flex items-center w-full p-1 cursor-pointer select-none hover:text-indigo-700">
       {/* TODO: style checkbox */}
       <input type="checkbox" />
       <span className="ml-1">Lorem, ipsum dolor sit</span>
